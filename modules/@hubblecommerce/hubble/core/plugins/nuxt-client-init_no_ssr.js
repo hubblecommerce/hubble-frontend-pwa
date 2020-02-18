f137474994e7bff48cab5e39cd8f197196ad3ddc
Lukas Heinze <heinze@digitalmanufaktur.com>
2020-02-18 14:05:29 +0100

    Removed redundant plugin

--- modules/@hubblecommerce/hubble/core/plugins/nuxt-client-init_no_ssr.js
+++
@@ -1,5 +0,0 @@
-export default async (ctx) => {
-    await ctx.store.dispatch('modCart/setByCookie', ctx);
-    await ctx.store.dispatch('modUser/setByCookie', ctx);
-    await ctx.store.dispatch('modWishlist/setByCookie', ctx);
-}
