1835405b244abe8f1be78de26863044a777f9de6
Lukas Heinze <heinze@digitalmanufaktur.com>
2020-02-18 14:22:51 +0100

    Removed unused components and files, since they moved to specific subdirectories for better overview

--- modules/@hubblecommerce/hubble/core/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-import Middleware from './middleware'
-
-// Register a new middleware with key 'hubbleware' to get used in pages or layouts
-Middleware.hubbleware = function ({ store, params, query }) {
-
-    //console.log("custom middleware by hubble");
-
-};
