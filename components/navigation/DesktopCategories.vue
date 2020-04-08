c932c182ae76536a3efb14ffe4ab5a5d315b410c
Neslihan Genctürk <neslihangencturk@neslihans-mbp.localdomain>
2020-04-08 19:33:39 +0200

    Refactoring: DesktopCategories.vue - add getApiLocale mapGetter, reference NOT updated, reformat file

--- components/navigation/DesktopCategories.vue
+++ components/navigation/DesktopCategories.vue
@@ -9,6 +9,7 @@
         <nuxt-link :to="itemUrlPath" v-if="depth !== 0" @mouseover.native="showChildren = true" class="trigger bg-white m-0 w-100 d-flex align-items-center">
             {{ dataItem.name }}
         </nuxt-link>
+
         <div class="sub-categories md-elevation-4" v-if="(showChildren || depth === 0 || depth > 1) && hasChildren" v-bind:class="{'sub-sub-category': depth > 1}">
             <desktop-categories
                 v-for="(node,index) of dataItem.children"
@@ -17,12 +18,14 @@
                 :key="node.id"
                 :firstItem="index === 0"
             >
+
             </desktop-categories>
         </div>
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
     export default {
         name: "DesktopCategories",
         props: {
@@ -47,35 +50,10 @@
             }
         },
 
-        methods: {
-            toggleChildren(nodes, target) {
-                if(nodes) {
-                    this.showChildren = !this.showChildren;
-                } else {
-                    // Go to target url if no subcategories exists
-                    // window.open(target);
-                }
-            },
-            closeSubcategory() {
-                if(this.depth != 0) {
-                    this.$parent.showChildren = false;
-                    this.showChildren = false;
-                }
-            },
-            showChildrenOnHover() {
-                this.showChildren = true;
-                this.$children.showChildren = true;
-            }
-        },
-
-        watch: {
-            '$route.path': function() {
-                // Close menu layer if route changes
-                this.showChildren = false;
-            }
-        },
-
         computed: {
+            ...mapGetters({
+                getApiLocale: 'modApiResources/getApiLocale'
+            }),
             hasChildren() {
                 return ! _.isEmpty(this.dataItem.children);
             },
@@ -100,6 +78,34 @@
                 }
 
                 return '/' + this.$parent.dataItem.url_path;
+            }
+        },
+
+        watch: {
+            '$route.path': function() {
+                // Close menu layer if route changes
+                this.showChildren = false;
+            }
+        },
+
+        methods: {
+            toggleChildren(nodes, target) {
+                if(nodes) {
+                    this.showChildren = !this.showChildren;
+                } else {
+                    // Go to target url if no subcategories exists
+                    // window.open(target);
+                }
+            },
+            closeSubcategory() {
+                if(this.depth != 0) {
+                    this.$parent.showChildren = false;
+                    this.showChildren = false;
+                }
+            },
+            showChildrenOnHover() {
+                this.showChildren = true;
+                this.$children.showChildren = true;
             }
         }
     }
