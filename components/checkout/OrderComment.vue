0932f45dea29fde271eef449cebc5eb9a7ef06df
Neslihan Genctürk <neslihangencturk@neslihans-mbp.localdomain>
2020-04-09 15:48:23 +0200

    Refactoring: OrderComment.vue - add mapMutation & update reference, format file

--- components/checkout/OrderComment.vue
+++ components/checkout/OrderComment.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="order-comment-wrp">
         <div class="comment-title headline-3" v-text="$t('Comment')" />
+
         <div class="text-small subline" v-text="$t('Add your comments to this order here:')" />
+
         <div class="hbl-input-group">
             <textarea id="comment" v-model="orderComment" :placeholder="$t('Your comment')" />
+
             <label for="comment" v-text="$t('Comment')" />
         </div>
     </div>
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
+    import {mapGetters, mapMutations} from 'vuex';
     export default {
         name: "OrderComment",
 
@@ -28,7 +31,7 @@
 
         watch: {
             orderComment: function(newValue) {
-                this.$store.commit('modApiPayment/setOrderComment', newValue);
+                this.setOrderCommentMutation(newValue);
             }
         },
 
@@ -37,6 +40,9 @@
         },
 
         methods: {
+            ...mapMutations({
+                setOrderCommentMutation: 'modApiPayment/setOrderComment'
+            }),
             setOrderComment: function() {
                 if(this.getOrderComment) {
                     this.orderComment = this.getOrderComment;
