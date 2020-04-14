5aed54141fdea5da87e7445db08c8955a6116041
Neslihan Genctürk <neslihangencturk@neslihans-mbp.localdomain>
2020-04-14 18:22:10 +0200

    Refactoring: AutocompleteList.vue - format file

--- components/search/AutocompleteList.vue
+++ components/search/AutocompleteList.vue
@@ -2,14 +2,31 @@
     <div class="autocomplete-list-wrp">
         <div v-if="showCategories">
             <div class="autocomplete-headline" v-text="$t('Categories')" />
-            <div v-for="(category, key) in categoryItems" :key="key" class="autocomplete-element-wrp" :class="{'selected-item': category.id === selectedItemId}">
-                <nuxt-link :to="'/' + category.url_path" class="autocomplete-element" v-text="category.heading_title" />
+
+            <div v-for="(category, key) in categoryItems"
+                 :key="key"
+                 class="autocomplete-element-wrp"
+                 :class="{'selected-item': category.id === selectedItemId}"
+            >
+                <nuxt-link :to="'/' + category.url_path"
+                           class="autocomplete-element"
+                           v-text="category.heading_title"
+                />
             </div>
         </div>
+
         <div v-if="showProducts">
             <div class="autocomplete-headline" v-text="$t('Products')" />
-            <div v-for="(product, key) in productItems" :key="key" class="autocomplete-element-wrp" :class="{'selected-item': product.id === selectedItemId}">
-                <nuxt-link :to="'/' + product.url_pds" class="autocomplete-element" v-text="product.manufacturer_name + ' ' + product.name" />
+
+            <div v-for="(product, key) in productItems"
+                 :key="key"
+                 class="autocomplete-element-wrp"
+                 :class="{'selected-item': product.id === selectedItemId}"
+            >
+                <nuxt-link :to="'/' + product.url_pds"
+                           class="autocomplete-element"
+                           v-text="product.manufacturer_name + ' ' + product.name"
+                />
             </div>
         </div>
     </div>
