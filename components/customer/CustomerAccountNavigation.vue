b3f05804b2ff410afcbfd7c5c7cc2bd58ea33843
Neslihan Genctürk <neslihangencturk@neslihans-mbp.localdomain>
2020-04-14 12:45:31 +0200

    Refactoring: CustomerAccountNavigation.vue - format file

--- components/customer/CustomerAccountNavigation.vue
+++ components/customer/CustomerAccountNavigation.vue
@@ -6,20 +6,24 @@
                 <material-ripple />
             </button>
         </nuxt-link>
+
         <nuxt-link :to="localePath('customer-orders')" class="sidebar-link">
             <button class="button-secondary">
                 {{ $t('My Orders') }}
                 <material-ripple />
             </button>
         </nuxt-link>
+
         <nuxt-link :to="localePath('customer-addresses')" class="sidebar-link">
             <button class="button-secondary">
                 {{ $t('Customer Addresses') }}
                 <material-ripple />
             </button>
         </nuxt-link>
+
         <button class="button-primary logout-button" @click.prevent="attemptLogout">
             {{ $t('Logout') }}
+
             <material-ripple />
         </button>
     </div>
@@ -27,8 +31,10 @@
 
 <script>
     import { mapActions} from 'vuex';
+
     export default {
         name: "CustomerAccountNavigation",
+
         methods: {
             ...mapActions({
                 logOut: 'modApiCustomer/logOut',
@@ -36,7 +42,7 @@
                 flashMessage: 'modFlash/flashMessage',
                 hideOffcanvasAction: 'modNavigation/hideOffcanvasAction'
             }),
-            attemptLogout() {
+            attemptLogout: function() {
                 let msg = this.$t('Successfully logged out');
                 this.logOut().then(() => {
                         // Clear wishlist from store
