119785e297a8a7d21bb05f64579879b50e377769
Neslihan Genctürk <neslihangencturk@neslihans-mbp.localdomain>
2020-04-14 17:33:37 +0200

    Refactoring: SelectableOrder.vue - format file

--- components/productlist/toolbar/SelectableOrder.vue
+++ components/productlist/toolbar/SelectableOrder.vue
@@ -2,11 +2,14 @@
     <div class="hbl-select">
         <select id="selectable-order" v-model="modelSelected" class="select-text" required>
             <option value="" disabled></option>
+
             <option v-for="(dataOption, index) in dataOptions" :key="index" :value="dataOption.option_id">
                 {{ $t(dataOption.label) }}
             </option>
         </select>
+
         <span class="select-highlight"></span>
+
         <label for="selectable-order" class="select-label" v-text="$t('Sort by')"></label>
     </div>
 </template>
@@ -16,13 +19,6 @@
 export default {
     name: 'SelectableOrder',
 
-    data() {
-        return {
-            name: 'SelectableOrder',
-            modelSelected: null
-        }
-    },
-
     props: {
         dataOptions: {
             type: Array,
@@ -30,14 +26,15 @@ export default {
         }
     },
 
-    created() {
-        if(this.$route.query.sort) {
-            this.modelSelected = this.$route.query.sort;
+    data() {
+        return {
+            name: 'SelectableOrder',
+            modelSelected: null
         }
     },
 
     watch: {
-        modelSelected(newValue, oldValue) {
+        modelSelected: function(newValue, oldValue) {
 
             // send payload on to the bus ...
             this.$bus.$emit('selectable-order-changed', {
@@ -46,6 +43,12 @@ export default {
                     data: newValue
                 }
             })
+        }
+    },
+
+    created() {
+        if(this.$route.query.sort) {
+            this.modelSelected = this.$route.query.sort;
         }
     }
 }
