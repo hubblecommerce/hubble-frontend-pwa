cb5bc691b710f46858c8c8e9f198c6a05462c70d
Michel Kix <kix@digitalmanufaktur.com>
2020-04-15 20:17:05 +0200

    updated linting and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  // @nuxtjs not used, because it extends eslint/standard
-  // prettier disabled for now
-  extends: [
-    'eslint:recommended',
-    'plugin:nuxt/recommended',
-    'plugin:vue/recommended',
-    /*'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-      'plugin:nuxt/recommended'*/
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'prettier',
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/html-indent': 'off',
-    'vue/max-attributes-per-line': 'off',
-    "vue/multiline-html-element-content-newline": 'off',
-    "vue/singleline-html-element-content-newline" : 'off',
-    "vue/no-v-html" : 'off',
-  },
-  globals: {
-    '_': true
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/recommended',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended'
+    ],
+    plugins: [
+        'vue'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'vue/html-indent': 'off',
+        'vue/max-attributes-per-line': 'off',
+        "vue/multiline-html-element-content-newline": 'off',
+        "vue/singleline-html-element-content-newline" : 'off',
+        "vue/no-v-html" : 'off',
+    },
+    globals: {
+        '_': true
+    }
 }
