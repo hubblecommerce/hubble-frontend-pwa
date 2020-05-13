c04f06674aaf4d5cf54033146f1c056c07e5cf2a
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-05-13 15:14:50 +0200

    HUBBLE-144: hubble_express.vue & hubble_light.vue: refactor other layouts to use slot implementation

--- layouts/hubble_express.vue
+++ layouts/hubble_express.vue
@@ -3,71 +3,74 @@
 
         <noscript>Please enable JavaScript and refresh this page, to use this application.</noscript>
 
-        <div v-if="$mq === 'sm' || $mq === 'md'" class="mobile-layout hubble-light" :class="{ disableScroll: activeOffCanvas }">
-            <background-blur />
-            <div class="header-wrp">
-                <div class="nav-wrp">
-                    <button v-if="$mq === 'sm'" class="button button-icon" @click="historyBack()">
-                        <i class="icon icon-arrow-left" />
-                        <div class="hidden-link-name">{{ $t('Back to shop') }}</div>
-                        <material-ripple />
-                    </button>
-                    <the-logo />
-                    <div v-if="$mq === 'md'" class="action-wrp d-flex align-items-center">
-                        <nuxt-link :to="localePath('index')">
-                            <button class="button-secondary">
-                                {{ $t('Back to shop') }}
-                                <material-ripple />
-                            </button>
-                        </nuxt-link>
-                    </div>
-                </div>
-                <flash-messages v-if="!activeOffCanvas" />
-            </div>
-
-            <main>
-                <nuxt />
-            </main>
-
-            <div class="footer hubble-light">
-                <the-footer-copyright-light />
-            </div>
-
-            <scroll-to-top />
-        </div>
-
-        <div v-if="$mq === 'lg'" class="desktop-layout hubble-light" :class="{ disableScroll: activeOffCanvas }">
-            <background-blur />
-            <div class="header-wrp">
-                <div class="nav-wrp">
-                    <the-logo />
-                    <div class="action-wrp d-flex align-items-center ml-auto">
-                        <nuxt-link :to="localePath('index')">
-                            <button class="button-secondary">
-                                {{ $t('Back to shop') }}
-                                <material-ripple />
-                            </button>
-                        </nuxt-link>
+        <layout-wrapper v-if="$mq === 'sm' || $mq === 'md'">
+            <div class="mobile-layout hubble-light">
+                <background-blur />
+                <div class="header-wrp">
+                    <div class="nav-wrp">
+                        <button v-if="$mq === 'sm'" class="button button-icon" @click="historyBack()">
+                            <i class="icon icon-arrow-left" />
+                            <div class="hidden-link-name">{{ $t('Back to shop') }}</div>
+                            <material-ripple />
+                        </button>
+                        <the-logo />
+                        <div v-if="$mq === 'md'" class="action-wrp d-flex align-items-center">
+                            <nuxt-link :to="localePath('index')">
+                                <button class="button-secondary">
+                                    {{ $t('Back to shop') }}
+                                    <material-ripple />
+                                </button>
+                            </nuxt-link>
+                        </div>
                     </div>
                     <flash-messages v-if="!activeOffCanvas" />
                 </div>
+
+                <main>
+                    <nuxt />
+                </main>
+
+                <div class="footer hubble-light">
+                    <the-footer-copyright-light />
+                </div>
+
+                <scroll-to-top />
+            </div>
+        </layout-wrapper>
+
+        <layout-wrapper v-if="$mq === 'lg'">
+            <div class="desktop-layout hubble-light">
+                <background-blur />
+                <div class="header-wrp">
+                    <div class="nav-wrp">
+                        <the-logo />
+                        <div class="action-wrp d-flex align-items-center ml-auto">
+                            <nuxt-link :to="localePath('index')">
+                                <button class="button-secondary">
+                                    {{ $t('Back to shop') }}
+                                    <material-ripple />
+                                </button>
+                            </nuxt-link>
+                        </div>
+                        <flash-messages v-if="!activeOffCanvas" />
+                    </div>
+                </div>
+
+                <main>
+                    <nuxt />
+                </main>
+
+                <div class="footer hubble-light">
+                    <the-footer-copyright-light />
+                </div>
             </div>
 
-            <main>
-                <nuxt />
-            </main>
-
-            <div class="footer hubble-light">
-                <the-footer-copyright-light />
-            </div>
-        </div>
-
-        <client-only>
-            <div v-if="showCookieNotice" class="cookie-notice">
-                <cookie-notice />
-            </div>
-        </client-only>
-
+            <client-only>
+                <div v-if="showCookieNotice" class="cookie-notice">
+                    <cookie-notice />
+                </div>
+            </client-only>
+        </layout-wrapper>
     </div>
 </template>
 
@@ -79,6 +82,8 @@
     import TheFooterCopyrightLight from "../components/footer/TheFooterCopyrightLight";
     import CookieNotice from "../components/utils/CookieNotice";
     import FlashMessages from "../components/utils/FlashMessages";
+    import LayoutWrapper from "../components/utils/LayoutWrapper";
+
     export default {
         components: {
             TheFooterCopyrightLight,
@@ -86,7 +91,8 @@
             ScrollToTop,
             TheLogo,
             CookieNotice,
-            FlashMessages
+            FlashMessages,
+            LayoutWrapper
         },
         computed: {
             ...mapState({
--- layouts/hubble_light.vue
+++ layouts/hubble_light.vue
@@ -3,72 +3,75 @@
 
         <noscript>Please enable JavaScript and refresh this page, to use this application.</noscript>
 
-        <div v-if="$mq === 'sm' || $mq === 'md'" class="mobile-layout hubble-light" :class="{ disableScroll: activeOffCanvas }">
-            <background-blur />
-            <div class="header-wrp">
-                <div class="nav-wrp">
-                    <button v-if="$mq === 'sm'" class="button button-icon" @click="historyBack()">
-                        <i class="icon icon-arrow-left" />
-                        <div class="hidden-link-name">{{ $t('Back to shop') }}</div>
-                        <material-ripple />
-                    </button>
-                    <the-logo />
-                    <div v-if="$mq === 'md'" class="action-wrp d-flex align-items-center">
-                        <nuxt-link :to="localePath('index')">
-                            <button class="button-secondary">
-                                {{ $t('Back to shop') }}
-                                <material-ripple />
-                            </button>
-                        </nuxt-link>
-                    </div>
-                </div>
-                <flash-messages v-if="!activeOffCanvas" />
-            </div>
-
-            <main>
-                <nuxt />
-            </main>
-
-            <div class="footer hubble-light">
-                <the-footer-copyright-light />
-            </div>
-
-            <scroll-to-top />
-        </div>
-
-        <div v-if="$mq === 'lg'" class="desktop-layout hubble-light" :class="{ disableScroll: activeOffCanvas }">
-            <background-blur />
-            <div class="header-wrp">
-                <div class="nav-wrp">
-                    <the-logo />
-                    <div class="action-wrp d-flex align-items-center ml-auto">
-                        <nuxt-link :to="localePath('index')">
-                            <button class="button-secondary">
-                                {{ $t('Back to shop') }}
-                                <material-ripple />
-                            </button>
-                        </nuxt-link>
+        <layout-wrapper v-if="$mq === 'sm' || $mq === 'md'">
+            <div class="mobile-layout hubble-light">
+                <background-blur />
+                <div class="header-wrp">
+                    <div class="nav-wrp">
+                        <button v-if="$mq === 'sm'" class="button button-icon" @click="historyBack()">
+                            <i class="icon icon-arrow-left" />
+                            <div class="hidden-link-name">{{ $t('Back to shop') }}</div>
+                            <material-ripple />
+                        </button>
+                        <the-logo />
+                        <div v-if="$mq === 'md'" class="action-wrp d-flex align-items-center">
+                            <nuxt-link :to="localePath('index')">
+                                <button class="button-secondary">
+                                    {{ $t('Back to shop') }}
+                                    <material-ripple />
+                                </button>
+                            </nuxt-link>
+                        </div>
                     </div>
                     <flash-messages v-if="!activeOffCanvas" />
                 </div>
+
+                <main>
+                    <nuxt />
+                </main>
+
+                <div class="footer hubble-light">
+                    <the-footer-copyright-light />
+                </div>
+
+                <scroll-to-top />
+            </div>
+        </layout-wrapper>
+
+        <layout-wrapper v-if="$mq === 'lg'">
+            <div class="desktop-layout hubble-light">
+                <background-blur />
+                <div class="header-wrp">
+                    <div class="nav-wrp">
+                        <the-logo />
+                        <div class="action-wrp d-flex align-items-center ml-auto">
+                            <nuxt-link :to="localePath('index')">
+                                <button class="button-secondary">
+                                    {{ $t('Back to shop') }}
+                                    <material-ripple />
+                                </button>
+                            </nuxt-link>
+                        </div>
+                        <flash-messages v-if="!activeOffCanvas" />
+                    </div>
+                </div>
+
+                <main>
+                    <checkout-progress-bar />
+                    <nuxt />
+                </main>
+
+                <div class="footer hubble-light">
+                    <the-footer-copyright-light />
+                </div>
             </div>
 
-            <main>
-                <checkout-progress-bar />
-                <nuxt />
-            </main>
-
-            <div class="footer hubble-light">
-                <the-footer-copyright-light />
-            </div>
-        </div>
-
-        <client-only>
-            <div v-if="showCookieNotice" class="cookie-notice">
-                <cookie-notice />
-            </div>
-        </client-only>
-
+            <client-only>
+                <div v-if="showCookieNotice" class="cookie-notice">
+                    <cookie-notice />
+                </div>
+            </client-only>
+        </layout-wrapper>
     </div>
 </template>
 
@@ -81,6 +84,8 @@
     import CheckoutProgressBar from "../components/checkout/CheckoutProgressBar";
     import CookieNotice from "../components/utils/CookieNotice";
     import FlashMessages from "../components/utils/FlashMessages";
+    import LayoutWrapper from "../components/utils/LayoutWrapper";
+
     export default {
         components: {
             CheckoutProgressBar,
@@ -89,7 +94,8 @@
             ScrollToTop,
             TheLogo,
             CookieNotice,
-            FlashMessages
+            FlashMessages,
+            LayoutWrapper
         },
         computed: {
             ...mapState({
