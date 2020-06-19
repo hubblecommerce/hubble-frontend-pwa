8a728837c71f326ec802bac26c96874b3e5864bb
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-06-19 12:50:11 +0200

    HUBBLE-121: add youtube slot & block

---
+++ components/swComponents/blocks/youtube-video.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="col-12">
+            <component :is="getSlot" :content="getContentByPosition(content.slots, 'video')" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import {blockMixins} from "../helper";
+
+    export default {
+        name: "YoutubeVideoBlock",
+
+        mixins: [blockMixins],
+
+        props: {
+            content: {
+                type: Object,
+                default: () => ({})
+            }
+        },
+
+        computed: {
+            getSlot() {
+                return this.getSlotByPosition(this.content.slots, 'video');
+            }
+        }
+    }
+</script>
---
+++ components/swComponents/slots/youtube-video.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="[elementClass, getDisplayModeClass]">
+        <iframe :src="getVideoUrl" class="cms-element-youtube-video__video"></iframe>
+    </div>
+</template>
+
+<script>
+    import { slotMixins } from '../helper'
+    export default {
+        name: "YoutubeVideoSlot",
+
+        mixins: [slotMixins],
+
+        props: {
+            content: {
+                type: Object,
+                default: () => ({})
+            }
+        },
+        computed: {
+            getVideoUrl() {
+                return `https://www.youtube-nocookie.com/embed/${this.content.config.videoID.value}?rel=0&${this.getAutoPlay}${this.getLoop}${this.getShowControls}${this.getStart}${this.getEnd}disablekb=1`;
+            },
+            getAutoPlay() {
+                return !this.content.config.autoPlay.value ? '' : 'autoplay=1&';
+            },
+            getLoop() {
+                return !this.content.config.loop.value ? '' : `loop=1&playlist=${this.getVideoID}&`;
+            },
+            getShowControls() {
+                return (this.content.config.showControls.value) ? '' : 'controls=0&';
+            },
+            getStart() {
+                return (!this.content.config.start.value) ? '' : `start=${this.content.config.start.value}&`;
+            },
+            getEnd() {
+                return !this.content.config.end.value ? '' : `end=${this.content.config.end.value}&`
+            },
+            getDisplayMode() {
+                return this.content.config.displayMode.value;
+            },
+            getDisplayModeClass() {
+                return this.getDisplayMode === 'standard' ? '' : `is-${this.getDisplayMode}`;
+            }
+        }
+    }
+</script>
