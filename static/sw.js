c0e8ff096235e06faac6bbc52cce98c1e4739b9b
dm-neslihan <genctuerk@digitalmanufaktur.com>
2020-08-12 23:44:59 +0200

    Hubble-93: update sw.js to new syntax #experimental

--- static/sw.js
+++ static/sw.js
@@ -6,42 +6,32 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         // create a new cache called v1
         // returns a promise for a created cache
-        caches.open(cacheVersion).then(function(cache) {
-            // call a function that calls addAll() on the created cache
-            // takes an array of origin-relative URLs to all the resources you want to cache.
-            return cache.addAll([
+        caches.open(cacheVersion)
+            .then((cache)  => cache.addAll([
                 '/offline.html',
                 '/launch-icon.svg'
-            ]);
-        })
-    );
+            ]))
+    )
 });
 
 // Respond with cached asset if network request fails
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        fetch(event.request).catch(function() {
-            caches.match('/offline.html');
-        })
+        fetch(event.request)
+            .catch(() => caches.match('/offline.html'))
     );
 });
 
 // Remove old Caches
 self.addEventListener('activate', function(event) {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    // Return true if you want to remove this cache,
-                    // but remember that caches are shared across
-                    // the whole origin
-                    if(cacheName != cacheVersion) {
-                        return true;
-                    }
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
+        caches.keys()
+            .then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames
+                        .filter((cacheName) => cacheName != cacheVersion)
+                        .map((cacheName) => caches.delete(cacheName))
+                );
         })
     );
 });
