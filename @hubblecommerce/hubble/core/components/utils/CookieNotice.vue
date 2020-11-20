1e73526b209e96c8e236a6fed2074690e7d92734
zermoran <z3rm0ran@gmail.com>
2020-11-20 16:15:38 +0100

    issue#10 changed text to be translatable, change tags to be self closing, removed redundacy by handling media queries in scss

--- @hubblecommerce/hubble/core/components/utils/CookieNotice.vue
+++ @hubblecommerce/hubble/core/components/utils/CookieNotice.vue
@@ -1,37 +1,21 @@
 <template>
-    <div class="row cookie-notice-wrp">
-        <template v-if="$mq === 'lg' || $mq === 'md'">
-            <div class="col-9 cookie-text-wrp">
+    <div class="row cookie-notice-outer-wrp">
+        <div class="cookie-notice-wrp">
+            <div class="cookie-text-wrp">
                 <div class="cookie-icon">
                     <i class="icon icon-cookie" />
                 </div>
 
-                <div class="text">
-                    Diese Website verwendet Cookies, um die Interaktion mit anderen Websites und sozialen Netzwerken zu ermöglichen sowie den Komfort
-                    bei der Benutzung dieser Website zu erhöhen. Details entnehmt Ihr bitte unseren Hinweisen zum
-                    <nuxt-link class="link-primary" :to="'/Datenschutz'">Datenschutz.</nuxt-link>
+                <div class="text cookie-text">
+                    {{ $t('Cookie text') }}
+                    <nuxt-link class="link-primary cookie-link" :to="'/Datenschutz'" v-text="$t('Data protection')" />
                 </div>
             </div>
-            <div class="col-3 cookie-notice-button-wrp">
-                <button class="float-right button button-primary" @click="deactivateCookieNotice" v-text="'Verstanden'" />
+
+            <div class="cookie-notice-button-wrp">
+                <button class="button button-primary cookie-notice-button" @click="deactivateCookieNotice" v-text="$t('Understood')" />
             </div>
-        </template>
-        <template v-if="$mq === 'sm'">
-            <div class="row cookie-notice-wrp">
-                <div class="row col-12 cookie-text-wrp">
-                    <div class="text">
-                        Diese Website verwendet Cookies, um die Interaktion mit anderen Websites und sozialen Netzwerken zu ermöglichen sowie den
-                        Komfort bei der Benutzung dieser Website zu erhöhen. Details entnehmt Ihr bitte unseren Hinweisen zum
-                        <nuxt-link class="link-primary cookie-link" :to="'/Datenschutz'">Datenschutz.</nuxt-link>
-                    </div>
-                </div>
-                <div class="row col-12 cookie-notice-button-wrp">
-                    <button class="button button-primary cookie-notice-button" @click="deactivateCookieNotice">
-                        Verstanden
-                    </button>
-                </div>
-            </div>
-        </template>
+        </div>
     </div>
 </template>
 
