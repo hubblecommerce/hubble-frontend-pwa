aa7ecffe0eb7a1cf9295f4dff4f367ea296b5526
Jonas Dally <dally@digitalmanufaktur.com>
2021-05-31 17:43:26 +0200

    Show arrow icon in footer collapsible menu

--- @hubblecommerce/hubble/core/components/utils/Collapsible.vue
+++ @hubblecommerce/hubble/core/components/utils/Collapsible.vue
@@ -4,11 +4,11 @@
             {{ toggleText }}
 
             <!--            <transition-rotate-x>-->
-            <i v-if="!collapse" :class="openIconClass" class="icon" />
+            <!-- <i v-if="!collapse" :class="openIconClass" class="icon" /> -->
             <!--            </transition-rotate-x>-->
 
             <!--            <transition-rotate-x>-->
-            <i v-if="collapse" :class="closeIconClass" class="icon" />
+            <!-- <i v-if="collapse" :class="closeIconClass" class="icon" /> -->
             <!--            </transition-rotate-x>-->
         </button>
 
@@ -97,7 +97,7 @@ export default {
             el.style.maxHeight = 0;
         },
         afterEnter: function (el) {
-            el.style.maxHeight = this.maxHeight.toString() + 'px';
+            el.style.maxHeight = el.scrollHeight + 'px';
         },
         beforeLeave: function (el) {
             el.style.maxHeight = '0';
@@ -113,6 +113,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import '~assets/scss/hubble/variables';
+
 .collapse-wrp {
     width: 100%;
 
@@ -126,6 +128,29 @@ export default {
             justify-content: flex-start;
             align-items: center;
             padding-right: 50px;
+        }
+
+        &.active {
+            &::after {
+                transform: translate3d(0, -50%, 0) rotate(90deg);
+            }
+        }
+
+        &::after {
+            content: "";
+            background-image: url("data:image/svg+xml,%3Csvg height='20' viewBox='0 0 20 20' width='20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath d='m3.29289322 6.29289322c.36048396-.36048396.92771502-.3882135 1.32000622-.08318861l.09420734.08318861 5.29289322 5.29210678 5.2928932-5.29210678c.360484-.36048396.927715-.3882135 1.3200062-.08318861l.0942074.08318861c.3604839.36048396.3882135.92771502.0831886 1.32000622l-.0831886.09420734-6 6.00000002c-.360484.3604839-.92771504.3882135-1.32000624.0831886l-.09420734-.0831886-6-6.00000002c-.39052429-.39052429-.39052429-1.02368927 0-1.41421356z' fill='%23000000' fill-rule='nonzero' transform='matrix(0 -1 1 0 0 20)'/%3E%3C/g%3E%3C/svg%3E%0A");
+            background-repeat: no-repeat;
+            background-position: center center;
+            display: inline-block;
+            width: 32px;
+            height: 32px;
+            border-radius: 16px;
+            text-align: center;
+            position: absolute;
+            right: $base-padding;
+            top: 50%;
+            transform: translate3d(0, -50%, 0);
+            transition: transform .2s;
         }
     }
 
