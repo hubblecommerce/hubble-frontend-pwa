7f5e77bda72632b968fdce83c722a7be70d02bbb
Jonas Dally <dally@digitalmanufaktur.com>
2021-06-02 12:11:27 +0200

    Include manufacturer again and show its name if no logo exists

--- @hubblecommerce/hubble/core/components/detail/ProductDetailManufacturer.vue
+++ @hubblecommerce/hubble/core/components/detail/ProductDetailManufacturer.vue
@@ -1,8 +1,15 @@
 <template>
-    <div v-if="dataProduct.manufacturer_item != null" class="brand-logo-wrp">
-        <img v-if="dataProduct.manufacturer_item.logo != null" :src="dataProduct.manufacturer_item.logo" :alt="dataProduct.manufacturer_item.name" />
+    <div
+        v-if="dataProduct.manufacturer_item != null && dataProduct.manufacturer_item.logo != null"
+        class="brand-logo-wrp"
+    >
+        <img
+            v-if="dataProduct.manufacturer_item.logo != null"
+            :src="dataProduct.manufacturer_item.logo"
+            :alt="dataProduct.manufacturer_item.name"
+        />
     </div>
-    <div v-else-if="dataProduct.manufacturer_item != null" class="manufacturer-name text-small" v-text="dataProduct.manufacturer_item.name" />
+    <div v-else-if="dataProduct.manufacturer_name != null" class="manufacturer-name text-small" v-text="dataProduct.manufacturer_name" />
 </template>
 
 <script>
