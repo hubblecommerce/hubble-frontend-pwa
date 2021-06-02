710cc12fb65508706c116bc55fe04d8d62cb3ea4
Jonas Dally <dally@digitalmanufaktur.com>
2021-06-02 11:17:25 +0200

    Replaced icon as background-image with svg component

--- @hubblecommerce/hubble/core/components/form/HblSelect.vue
+++ @hubblecommerce/hubble/core/components/form/HblSelect.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="hbl-select">
         <slot />
+
+        <div class="icon-wrp">
+            <svg-icon icon="chevron-down" size="sm" />
+        </div>
     </div>
 </template>
 
@@ -13,20 +17,6 @@ export default {
 <style lang="scss">
 @import '~assets/scss/hubble/variables';
 @import '~assets/scss/hubble/typography';
-
-/*
-<div class="hbl-select">
-    <select class="select-text" required>
-        <option value="" disabled selected></option>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
-    </select>
-    <span class="select-highlight"></span>
-    <span class="select-bar"></span>
-    <label class="select-label">Select</label>
-</div>
-*/
 
 $hbl-select-height: 42px;
 $hbl-select-height-md: 42px;
@@ -48,23 +38,21 @@ $hbl-select-hover-border: 1px solid $darker-gray;
     margin-bottom: 20px;
     background: $hbl-select-background-color;
 
-    &:after {
-        content: '';
-        background-image:  url("data:image/svg+xml,%3Csvg height='20' viewBox='0 0 20 20' width='20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23212a49' fill-rule='evenodd'%3E%3Cpath d='m3.29289322 6.29289322c.36048396-.36048396.92771502-.3882135 1.32000622-.08318861l.09420734.08318861 5.29289322 5.29210678 5.2928932-5.29210678c.360484-.36048396.927715-.3882135 1.3200062-.08318861l.0942074.08318861c.3604839.36048396.3882135.92771502.0831886 1.32000622l-.0831886.09420734-6 6.00000002c-.360484.3604839-.92771504.3882135-1.32000624.0831886l-.09420734-.0831886-6-6.00000002c-.39052429-.39052429-.39052429-1.02368927 0-1.41421356z' fill='%231b1b24' fill-rule='nonzero'/%3E%3C/g%3E%3C/svg%3E%0A");
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: 16px;
-        color: $hbl-select-label-color;
-
+    .icon-wrp {
         position: absolute;
         right: 0;
         top: 0;
         width: $hbl-select-height;
         height: $hbl-select-height;
-        line-height: $hbl-select-height;
-        text-align: center;
         z-index: 0;
         border-left: $hbl-select-border;
+
+        .icon {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
     }
 
     .select-text {
@@ -193,7 +181,7 @@ $hbl-select-hover-border: 1px solid $darker-gray;
         }
 
         &:hover {
-            &:after {
+            .icon-wrp {
                 border-left: $hbl-select-hover-border;
             }
 
--- @hubblecommerce/hubble/core/components/utils/Collapsible.vue
+++ @hubblecommerce/hubble/core/components/utils/Collapsible.vue
@@ -1,15 +1,8 @@
 <template>
     <div v-click-outside="hideContent" class="collapse-wrp">
         <button class="toggle m-0 align-left headline-4" :class="toggleClasses" @mousedown.prevent="collapseContent()">
-            {{ toggleText }}
-
-            <!--            <transition-rotate-x>-->
-            <!-- <i v-if="!collapse" :class="openIconClass" class="icon" /> -->
-            <!--            </transition-rotate-x>-->
-
-            <!--            <transition-rotate-x>-->
-            <!-- <i v-if="collapse" :class="closeIconClass" class="icon" /> -->
-            <!--            </transition-rotate-x>-->
+            <span>{{ toggleText }}</span>
+            <svg-icon icon="chevron-right" />
         </button>
 
         <template v-if="displayViaIf">
@@ -131,32 +124,20 @@ export default {
         }
 
         &.active {
-            &::after {
-                transform: translate3d(0, -50%, 0) rotate(90deg);
+            .icon {
+                transform: rotate(90deg);
             }
-        }
-
-        &::after {
-            content: "";
-            background-image: url("data:image/svg+xml,%3Csvg height='20' viewBox='0 0 20 20' width='20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath d='m3.29289322 6.29289322c.36048396-.36048396.92771502-.3882135 1.32000622-.08318861l.09420734.08318861 5.29289322 5.29210678 5.2928932-5.29210678c.360484-.36048396.927715-.3882135 1.3200062-.08318861l.0942074.08318861c.3604839.36048396.3882135.92771502.0831886 1.32000622l-.0831886.09420734-6 6.00000002c-.360484.3604839-.92771504.3882135-1.32000624.0831886l-.09420734-.0831886-6-6.00000002c-.39052429-.39052429-.39052429-1.02368927 0-1.41421356z' fill='%23000000' fill-rule='nonzero' transform='matrix(0 -1 1 0 0 20)'/%3E%3C/g%3E%3C/svg%3E%0A");
-            background-repeat: no-repeat;
-            background-position: center center;
-            display: inline-block;
-            width: 32px;
-            height: 32px;
-            border-radius: 16px;
-            text-align: center;
-            position: absolute;
-            right: $base-padding;
-            top: 50%;
-            transform: translate3d(0, -50%, 0);
-            transition: transform .2s;
         }
     }
 
     .icon {
         position: absolute;
         right: 20px;
+        transition: transform .2s;
+
+        > svg {
+            top: auto;
+        }
     }
 
     .collapse-item {
