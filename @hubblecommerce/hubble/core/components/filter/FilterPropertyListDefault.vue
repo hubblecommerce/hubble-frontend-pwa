b88d4d4b768c867deafe2ee67404631159ae166b
Jonas Dally <dally@digitalmanufaktur.com>
2021-06-29 12:18:48 +0200

    Show count of checked filter in single property list

--- @hubblecommerce/hubble/core/components/filter/FilterPropertyListDefault.vue
+++ @hubblecommerce/hubble/core/components/filter/FilterPropertyListDefault.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <span v-if="selectedFiltersCount > 0">{{ selectedFiltersCount }} selected</span>
+        
         <div v-for="option in options" :key="option.id">
             <label :for="'option-' + option.id">
                 <input
@@ -30,19 +32,43 @@ export default {
         }
     },
 
+    data() {
+        return {
+            currentFilters: []
+        }
+    },
+
     computed: {
         ...mapState({
             properties: state => state.modFilter.properties,
             manufacturer: state => state.modFilter.manufacturer
         }),
+        selectedFiltersCount() {
+            return this.currentFilters.length;
+        }
+    },
+
+    mounted() {
+        this.getCurrentFilters();
     },
 
     methods: {
         getIsChecked(id) {
             return this[this.type] && this[this.type].includes(id);
         },
+        getCurrentFilters() {
+            this.currentFilters = this.options
+                .filter(option => this.getIsChecked(option.id))
+                .map(option => option.id);
+        },
         onChangeOption(id) {
             const type = this.type.charAt(0).toUpperCase() + this.type.slice(1);
+
+            if (this.currentFilters.includes(id)) {
+                this.currentFilters = this.currentFilters.filter(v => v !== id)
+            } else {
+                this.currentFilters = [...this.currentFilters, id];
+            }
             
             this.$store.commit(`modFilter/update${type}`, id);
         }
