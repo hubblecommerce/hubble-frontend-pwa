0333894403c6369d12327f2c65478002cc486374
Lukas Heinze <heinze@digitalmanufaktur.com>
2021-07-06 09:43:09 +0200

    Revert "Show count of checked filter in single property list"
    This reverts commit 32416d50ce661ca60b58d8a277df34de73fe35cd.

--- @hubblecommerce/hubble/core/components/filter/FilterPropertyListDefault.vue
+++ @hubblecommerce/hubble/core/components/filter/FilterPropertyListDefault.vue
@@ -1,7 +1,5 @@
 <template>
     <div>
-        <span v-if="selectedFiltersCount > 0">{{ selectedFiltersCount }} selected</span>
-        
         <div v-for="option in options" :key="option.id">
             <label :for="'option-' + option.id">
                 <input
@@ -32,43 +30,19 @@ export default {
         }
     },
 
-    data() {
-        return {
-            currentFilters: []
-        }
-    },
-
     computed: {
         ...mapState({
             properties: state => state.modFilter.properties,
             manufacturer: state => state.modFilter.manufacturer
         }),
-        selectedFiltersCount() {
-            return this.currentFilters.length;
-        }
-    },
-
-    mounted() {
-        this.getCurrentFilters();
     },
 
     methods: {
         getIsChecked(id) {
             return this[this.type] && this[this.type].includes(id);
         },
-        getCurrentFilters() {
-            this.currentFilters = this.options
-                .filter(option => this.getIsChecked(option.id))
-                .map(option => option.id);
-        },
         onChangeOption(id) {
             const type = this.type.charAt(0).toUpperCase() + this.type.slice(1);
-
-            if (this.currentFilters.includes(id)) {
-                this.currentFilters = this.currentFilters.filter(v => v !== id)
-            } else {
-                this.currentFilters = [...this.currentFilters, id];
-            }
             
             this.$store.commit(`modFilter/update${type}`, id);
         }
