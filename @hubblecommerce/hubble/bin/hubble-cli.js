a06cbf3355e57daeba0edd8e046abc96a8226db1
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-02-24 16:01:22 +0100

    hubble-cli: catch MODULE_NOT_FOUND error

--- @hubblecommerce/hubble/bin/hubble-cli.js
+++ @hubblecommerce/hubble/bin/hubble-cli.js
@@ -5,6 +5,10 @@ const args = process.argv.slice(2);
 try {
     require(`@hubblecommerce/hubble/scripts/${args[0]}.js`);
 } catch (e) {
-    console.log(`Invalid argument: ${args[0]}`);
-    console.log('Please provide one of the scripts inside @hubblecommerce/hubble/script');
+    if(e.code === 'MODULE_NOT_FOUND') {
+        console.log(`Invalid argument: ${args[0]}`);
+        console.log('Please provide one of the scripts inside @hubblecommerce/hubble/scripts');
+    } else {
+        console.log(e.code);
+    }
 }
