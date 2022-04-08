ef5076c0ba8efb2e339f5d50b1548b7f5d78ab48
Jonas Dally <dally@digitalmanufaktur.com>
2022-04-08 10:51:04 +0200

    Load binded data if exist on PDS

--- @hubblecommerce/hubble/core/components/swComponents/slots/product-name-slot.vue
+++ @hubblecommerce/hubble/core/components/swComponents/slots/product-name-slot.vue
@@ -1,13 +1,13 @@
 <template>
     <div :class="[elementClass, { 'has-vertical-alignment': verticalAlign }]">
         <template v-if="verticalAlign">
-            <h2 v-if="product" class="cms-element-alignment" :class="verticalAlign" v-text="product.name" />
+            <h2 v-if="isMapped" class="cms-element-alignment" :class="verticalAlign" v-text="content.data.content" />
 
             <div v-else class="cms-element-alignment" :class="verticalAlign" v-html="rawHtml" />
         </template>
 
         <template v-else>
-            <h2 v-if="product" v-html="product.name" />
+            <h2 v-if="isMapped" v-text="content.data.content" />
 
             <div v-else v-html="rawHtml" />
         </template>
@@ -22,6 +22,9 @@ export default {
     mixins: [slotMixins],
 
     computed: {
+        isMapped() {
+            return this.content && this.content.config && this.content.config.content && this.content.config.content.source === 'mapped';
+        },
         rawHtml() {
             return this.content && this.content.data && this.content.data.content;
         },
