051eb8008745bfbcd5491e6f601284b576bb0128
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-06-13 09:15:06 +0200

    Added custom build config to create js files of src/runtime/src/public

---
+++ build.config.ts
@@ -0,0 +1,11 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+    declaration: true,
+    rollup: { cjsBridge: true },
+    entries: [
+        './src/module',
+        { input: 'src/runtime/', outDir: 'dist/runtime' },
+        { input: 'src/runtime/src/public/', outDir: 'dist/runtime/src/public', ext: 'js' }
+    ]
+})
