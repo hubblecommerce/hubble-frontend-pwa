a5f2ac9fb8d5892073af2078c1174b512fa3e8b8
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-07-28 16:32:00 +0200

    Adjusted test fixture setup to new inheritance

--- __tests__/module/fixture/nuxt.config.ts
+++ __tests__/module/fixture/nuxt.config.ts
@@ -1,8 +1,16 @@
+import { fileURLToPath } from 'url'
 import { defineNuxtConfig } from 'nuxt'
 import hubble from '../../..'
 
 export default defineNuxtConfig({
     modules: [
         hubble
-    ]
+    ],
+    vite: {
+        resolve: {
+            alias: {
+                '@hubblecommerce/hubble': fileURLToPath(new URL('../../../src/runtime', import.meta.url))
+            }
+        }
+    }
 })
