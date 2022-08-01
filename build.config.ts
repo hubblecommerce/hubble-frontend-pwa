25fb889dc39419ed0cf445dd9bc75855f3f9125b
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-08-01 15:41:45 +0200

    Adjusted build: build platform bin-scripts as js to make dynamic import work

--- build.config.ts
+++ build.config.ts
@@ -7,6 +7,7 @@ export default defineBuildConfig({
         './src/module',
         { input: 'src/runtime/commons/', outDir: 'dist/runtime/commons', declaration: true },
         { input: 'src/runtime/platforms/shopware/api-client/generated/', outDir: 'dist/runtime/platforms/shopware/api-client/generated', declaration: true },
+        { input: 'src/runtime/platforms/shopware/bin/', outDir: 'dist/runtime/platforms/shopware/bin', declaration: false, ext: 'js' },
         { input: 'src/runtime/src/public/', outDir: 'dist/runtime/src/public', ext: 'js' }
     ]
 })
