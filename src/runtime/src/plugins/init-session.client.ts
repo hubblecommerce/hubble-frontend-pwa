1c0394a2826e0ba1f8bc90640a5eda728d4049b4
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-08-04 16:38:00 +0200

    Clientside: try to get customer if sessiontoken exists

--- src/runtime/src/plugins/init-session.client.ts
+++ src/runtime/src/plugins/init-session.client.ts
@@ -1,12 +1,24 @@
 import { defineNuxtPlugin, useRuntimeConfig, useCookie } from '#app'
-import { usePlatform } from '#imports'
+import { useCustomer, usePlatform } from '#imports'
 
 export default defineNuxtPlugin(() => {
     const { sessionCookie } = useRuntimeConfig()
     const cookie = useCookie(sessionCookie.name)
     const { setSessionToken } = usePlatform()
 
-    if (cookie.value !== undefined) {
-        setSessionToken(cookie.value)
+    if (cookie.value === undefined) {
+        return
+    }
+
+    setSessionToken(cookie.value)
+
+    // Fetch potential customer if session cookie exists
+    const { getCustomer, isGuest, customer } = useCustomer()
+
+    // @ts-ignore
+    const { refresh } = getCustomer()
+
+    if (customer.value != null) {
+        refresh()
     }
 })
