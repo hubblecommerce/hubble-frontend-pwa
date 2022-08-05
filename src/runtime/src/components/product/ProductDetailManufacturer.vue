285ec2f2812079a5007a4b54927c1bfb7d04b69e
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-08-05 07:56:58 +0200

    Catch missing manufacturer media

--- src/runtime/src/components/product/ProductDetailManufacturer.vue
+++ src/runtime/src/components/product/ProductDetailManufacturer.vue
@@ -1,13 +1,18 @@
 <template>
-    <a :href="manufacturer.link" target="_blank" rel="noopener">
-        <img :src="manufacturer.media.url" :alt="manufacturer.name" class="ml-auto" style="width: 80px; height: auto;">
+    <a v-if="manufacturer.media" :href="manufacturer.link" target="_blank" rel="noopener">
+        <img :src="manufacturer.media.url" :alt="manufacturer.name" class="ml-auto" :style="`width: ${logoWidth}px; height: auto;`">
     </a>
 </template>
 
 <script setup lang="ts">
 import { Manufacturer } from '@hubblecommerce/hubble/commons'
 
-defineProps<{
-    manufacturer: Manufacturer
-}>()
+export interface ProductDetailManufacturerProps {
+    manufacturer: Manufacturer,
+    logoWidth?: number
+}
+
+withDefaults(defineProps<ProductDetailManufacturerProps>(), {
+    logoWidth: 80
+})
 </script>
