a1126be39f31efa20406b1ea62dcdc603145af38
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-08-10 16:18:17 +0200

    Removed unused code

--- src/runtime/src/components/misc/MiscSkeleton.vue
+++ src/runtime/src/components/misc/MiscSkeleton.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps } from 'vue'
+import { computed } from 'vue'
 import { ProductDetailManufacturerProps } from '../product/ProductDetailManufacturer.vue'
 
 interface Props {
