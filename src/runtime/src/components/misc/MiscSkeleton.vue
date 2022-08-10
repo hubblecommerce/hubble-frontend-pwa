93dd04bcd0916a6ee854f01a0cdd3fc22314eeb9
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-08-10 14:39:54 +0200

    MiscSkeleton.vue: created component to generate sekeleton loading placeholder

---
+++ src/runtime/src/components/misc/MiscSkeleton.vue
@@ -0,0 +1,106 @@
+<template>
+    <div
+        v-for="index in repeat"
+        :key="index"
+        class="bg-base-300"
+        :class="classes"
+        :style="styles"
+    />
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+import { ProductDetailManufacturerProps } from '../product/ProductDetailManufacturer.vue'
+
+interface Props {
+    text?: boolean,
+    round?: boolean,
+    circle?: boolean,
+    height?: string | number,
+    width?: string | number,
+    size?: 'small' | 'medium' | 'large',
+    repeat?: string | number,
+    animated?: boolean,
+    sharp?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    text: false,
+    round: false,
+    circle: false,
+    size: 'medium',
+    repeat: 1,
+    animated: true,
+    sharp: true
+})
+
+const styles = computed(() => {
+    let text
+    if (props.text) {
+        text = 'display: inline-block;'
+    }
+
+    let width = 'width: 100%;'
+    if (props.width) {
+        if (typeof props.width === 'number') {
+            width = `width: ${props.width}px;`
+        }
+        if (typeof props.width === 'string') {
+            width = `width: ${props.width};`
+        }
+    }
+
+    let height = 'height: 24px;'
+    if (props.size === 'small') {
+        height = 'height: 24px;'
+    }
+
+    if (props.size === 'medium') {
+        height = 'height: 32px;'
+    }
+
+    if (props.size === 'large') {
+        height = 'height: 48px;'
+    }
+
+    if (props.circle) {
+        width = height.replace('height', 'width')
+    }
+
+    return [
+        text,
+        width,
+        height
+    ].join(' ')
+})
+
+const classes = computed(() => {
+    let round
+    if (props.round) {
+        round = 'rounded-3xl'
+    }
+
+    if (!props.sharp) {
+        round = 'rounded'
+    }
+
+    if (props.circle) {
+        round = 'rounded-full'
+    }
+
+    let animated
+    if (props.animated) {
+        animated = 'animate-pulse'
+    }
+
+    return [
+        round,
+        animated
+    ].join(' ')
+})
+
+</script>
+
+<style>
+
+</style>
