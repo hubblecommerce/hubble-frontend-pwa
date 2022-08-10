f441726cb558ebaf9a44d196ff9320ad0cbf1e25
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-08-10 16:19:33 +0200

    ProductListing: added props to control components behaviour

--- src/runtime/src/components/product/ProductListing.vue
+++ src/runtime/src/components/product/ProductListing.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex justify-between mb-4">
-        <div>
+        <div v-if="total">
             Total: {{ data.total }}
         </div>
-        <div>
+        <div v-if="pagination">
             Page {{ data.page }} of {{ Math.ceil(data.total / data.limit) }}
         </div>
     </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div :class="gridClasses">
         <ProductListingCard v-for="product in data.products" :key="product.id" :data="product" />
     </div>
 </template>
@@ -16,7 +16,16 @@
 <script setup lang="ts">
 import { ProductListing } from '@hubblecommerce/hubble/commons'
 
-const props = defineProps<{
-    data: ProductListing
-}>()
+interface ProductListingProps {
+    data: ProductListing,
+    total?: boolean,
+    pagination?: boolean,
+    gridClasses?: string
+}
+
+withDefaults(defineProps<ProductListingProps>(), {
+    total: true,
+    pagination: true,
+    gridClasses: 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
+})
 </script>
