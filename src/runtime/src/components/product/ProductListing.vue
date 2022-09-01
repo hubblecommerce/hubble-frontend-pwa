8f59024bd3db0ca9edf24d23a63deac943f36acf
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-09-01 15:53:09 +0200

    ProductListing: edited gird-cols for larger screens

--- src/runtime/src/components/product/ProductListing.vue
+++ src/runtime/src/components/product/ProductListing.vue
@@ -67,7 +67,7 @@ const props = withDefaults(defineProps<ProductListingProps>(), {
     total: true,
     pagination: true,
     sorting: true,
-    gridClasses: 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
+    gridClasses: 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4'
 })
 
 const listing = ref(props.data)
