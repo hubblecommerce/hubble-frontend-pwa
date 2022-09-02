264d92229c83c605ccf5971f4f95cb278746a599
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-09-02 15:51:07 +0200

    Listing: added add to cart function

--- src/runtime/src/components/product/ProductListingCard.vue
+++ src/runtime/src/components/product/ProductListingCard.vue
@@ -1,24 +1,34 @@
 <template>
-    <div class="card card-compact bg-base-100 border border-base-200 shadow-md">
+    <div class="card card-compact bg-base-100 border border-base-200 shadow-md transition hover:shadow-xl">
         <figure>
             <NuxtLink :to="data.url">
                 <img v-if="isLoading || data.media == null" src="~/assets/product/placeholder-image.png" class="d-block m-auto mw-100" height="300" :alt="data.name">
                 <img v-else :src="image.src" :alt="data.name">
             </NuxtLink>
         </figure>
-        <div class="card-body justify-between">
-            <h2 class="card-title" v-text="data.name" />
+        <div class="card-body justify-between gap-4">
+            <h2 class="card-title items-start" v-text="data.name" />
             <div class="card-text">
-                <div class="price">
-                    <span v-if="data.price?.specialPrice" class="special-price" v-text="formatPrice(data.price.specialPrice)" />
-                    <span :class="{ 'text-decoration-line-through': hasSpecialPrice }" v-text="formatPrice(data.price?.regularPrice)" />
+                <div class="flex gap-2 items-center text-base">
+                    <div v-if="data.price.regularPrice" :class="{ 'text-secondary': data.price.specialPrice}">
+                        {{ formatPrice(data.price.regularPrice) }}
+                    </div>
+                    <div v-if="data.price.specialPrice" class="line-through text-sm">
+                        {{ formatPrice(data.price.specialPrice) }}
+                    </div>
                 </div>
             </div>
             <div class="card-actions justify-center xl:justify-between">
                 <NuxtLink :to="data.url" class="btn btn-primary text-center" role="button">
                     Details
                 </NuxtLink>
-                <button class="btn btn-secondary text-center" role="button">
+                <button
+                    :class="{ 'loading': cartLoading }"
+                    :disabled="cartLoading"
+                    class="btn btn-secondary text-center"
+                    role="button"
+                    @click="onAddToCart()"
+                >
                     Add To Cart
                 </button>
             </div>
@@ -27,9 +37,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useImage } from '@vueuse/core'
+import { navigateTo } from '#app'
 import { Product, useCurrency } from '@hubblecommerce/hubble/commons'
+import { useCart, useNotification } from '#imports'
 
 const props = defineProps<{
     data: Product
@@ -45,4 +57,25 @@ const image = ref({
     src: Array.isArray(props.data.media) ? props.data.media[0].url : props.data.media.url
 })
 const { isLoading } = useImage(image)
+
+const { addToCart, loading: cartLoading, error: cartError } = useCart()
+async function onAddToCart () {
+    if (props.data.parentId != null) {
+        await navigateTo(props.data.url)
+        return
+    }
+
+    await addToCart(1, props.data.id)
+}
+
+const { showNotification } = useNotification()
+watch(cartError, (val) => {
+    showNotification(val as string, 'error', true)
+})
 </script>
+
+<style scoped>
+.card-title {
+    min-height: 84px;
+}
+</style>
