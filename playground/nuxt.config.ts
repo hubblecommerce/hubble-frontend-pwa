0bb0321c2d06de5f055d39d9c5802caa5401dbdb
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-09-16 13:26:09 +0200

    Performance: deactivate inline styled, enabled asset compression, avoid prefetching all components

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,12 +1,28 @@
 import { fileURLToPath } from 'url'
-import { defineNuxtConfig } from 'nuxt'
 import eslintPlugin from 'vite-plugin-eslint'
 import hubble from '..'
 
+// @ts-ignore
 export default defineNuxtConfig({
     modules: [
         hubble
     ],
+    experimental: {
+        // https://github.com/nuxt/framework/issues/7517
+        treeshakeClientOnly: false,
+        // If components: global = true => all styles would be used inline. To avoid this use:
+        inlineSSRStyles: false
+    },
+    nitro: {
+        // https://github.com/unjs/nitro/pull/449
+        compressPublicAssets: true
+    },
+    components: {
+        // Register all your components globally, which will create async chunks for all your components
+        // and prevent global prefetching
+        global: true,
+        dirs: ['~/components']
+    },
     vite: {
         plugins: [
             eslintPlugin()
