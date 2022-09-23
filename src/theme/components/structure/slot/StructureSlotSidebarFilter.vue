68c27945c1e8f1ebcb07e31fb2f168fa63c89135
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-09-23 09:43:44 +0200

    ListingFilter: added placeholder and transition to prevent cls

--- src/theme/components/structure/slot/StructureSlotSidebarFilter.vue
+++ src/theme/components/structure/slot/StructureSlotSidebarFilter.vue
@@ -1,11 +1,24 @@
 <template>
-    <portal-target name="productListingFilter" />
+    <Transition name="fade" mode="out-in">
+        <div v-if="!mounted">
+            <MiscSkeleton size="medium" :repeat="1" />
+        </div>
+        <div v-else>
+            <portal-target name="productListingFilter" />
+        </div>
+    </Transition>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import { Slot } from '@hubblecommerce/hubble/commons'
 
 const props = defineProps<{
     content: Slot
 }>()
+
+const mounted = ref(false)
+onMounted(() => {
+    mounted.value = true
+})
 </script>
