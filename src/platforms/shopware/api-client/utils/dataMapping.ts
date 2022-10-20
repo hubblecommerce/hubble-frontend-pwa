df4d36530537ff0097d51740123c417f112b3c0f
Lukas Heinze <heinze@digitalmanufaktur.com>
2022-10-20 11:36:35 +0200

    SW API Client: catch missing tax info in mapPrice

--- src/platforms/shopware/api-client/utils/dataMapping.ts
+++ src/platforms/shopware/api-client/utils/dataMapping.ts
@@ -106,8 +106,8 @@ function mapPrice (calculatedPrice): Price {
     return {
         regularPrice: calculatedPrice?.unitPrice,
         specialPrice: calculatedPrice?.listPrice?.price,
-        tax: calculatedPrice?.calculatedTaxes[0].tax,
-        taxRate: calculatedPrice?.calculatedTaxes[0].taxRate
+        tax: calculatedPrice?.calculatedTaxes[0]?.tax,
+        taxRate: calculatedPrice?.calculatedTaxes[0]?.taxRate
     }
 }
 
