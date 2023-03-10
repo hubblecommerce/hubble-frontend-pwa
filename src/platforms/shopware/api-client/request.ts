f75d440dd000a1a67234e9aff3ceaeb821f3925e
Lukas Heinze <heinze@digitalmanufaktur.com>
2023-03-10 14:57:23 +0100

    Fixed unchecked object property access, edited platformLanuguages condition

--- src/platforms/shopware/api-client/request.ts
+++ src/platforms/shopware/api-client/request.ts
@@ -141,13 +141,13 @@ export const request = <T>(config: OpenAPIConfig, options: ApiRequestOptions): C
         }
     }
 
-    if(session.value.sessionToken !== null) {
-        platformHeaders['sw-context-token'] = session.value.sessionToken
+    if(session?.value?.sessionToken != null) {
+        platformHeaders['sw-context-token'] = session?.value?.sessionToken
     }
 
     const locale = app.vueApp.config.globalProperties.$i18n?.locale
 
-    if(platformLanguages?.length > 0 && locale) {
+    if(platformLanguages != null && platformLanguages?.length > 0 && locale) {
         const matchingLang = platformLanguages.find((lang: any) => {
             return lang.route === locale
         })
