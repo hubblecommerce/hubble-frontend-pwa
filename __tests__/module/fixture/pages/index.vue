bb2a739a08ea51a523d504e4e299c883586ce990
Lukas Heinze <heinze@digitalmanufaktur.com>
2023-06-20 16:24:45 +0200

    issue#142 enhanced module tests for platform plugins, theme inheritance, i18n, error pages

--- __tests__/module/fixture/pages/index.vue
+++ __tests__/module/fixture/pages/index.vue
@@ -6,13 +6,36 @@
         <div>Load composable from appropriate platform: {{ apiUrl }}</div>
         <div>Load overridden composable from project root: {{ cart }}</div>
         <div>RuntimeConfig | meta.category.title: {{ config.meta.category.title }}</div>
+        <div>RuntimeConfig | testPluginConfig1: {{ config.testPluginConfig1 }}</div>
+        <div>RuntimeConfig | testPluginConfig2: {{ config.testPluginConfig2 }}</div>
         <MiscModuleTestComponent />
         <ProductDetail />
+        <ProductDetailPlugin />
+        <div id="text" class="text-black">
+            This text is supposed to be black
+        </div>
+        <div>{{ t('index.translationTest') }}</div>
+        <MiscPluginSlot name="test-plugin-slot" />
     </div>
 </template>
 
 <script setup>
+import { useI18n } from 'vue-i18n'
+import { useCart, usePlatform } from '#imports'
+
 const { apiUrl } = usePlatform()
 const { cart } = useCart()
-const config = useRuntimeConfig()
+const config = useRuntimeConfig().public
+const { t } = useI18n()
 </script>
+
+<i18n>
+{
+    "en": {
+        "index.translationTest": "Test translation"
+    },
+    "de": {
+        "index.translationTest": "Test Übersetzung"
+    }
+}
+</i18n>
--- __tests__/module/module.spec.ts
+++ __tests__/module/module.spec.ts
@@ -1,12 +1,19 @@
 import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
-import { setup, $fetch } from '@nuxt/test-utils-edge'
+import { setup, $fetch, fetch } from '@nuxt/test-utils-edge'
+
+async function throw404 () {
+    return await $fetch('/this-route-not-exists')
+}
 
 describe('hubble Nuxt.js module setup', async () => {
     await setup({
         rootDir: fileURLToPath(new URL('./fixture', import.meta.url))
     })
 
+    /*
+     * Files based inheritance
+     */
     it('loads composables from appropriate platform', async () => {
         const html = await $fetch('/')
 
@@ -17,12 +24,6 @@ describe('hubble Nuxt.js module setup', async () => {
         const html = await $fetch('/')
 
         expect(html).toContain('Load overridden composable from project root: overridden component value')
-    })
-
-    it('injects runtime config from modules default', async () => {
-        const html = await $fetch('/')
-
-        expect(html).toContain('RuntimeConfig | meta.category.title: Category - Hubble Demo')
     })
 
     it('imports components from module', async () => {
@@ -37,11 +38,75 @@ describe('hubble Nuxt.js module setup', async () => {
         expect(html).toContain('Overridden component from project root')
     })
 
-    async function throw404 () {
-        return await $fetch('/this-route-not-exists')
-    }
+    /*
+     * Plugins
+     */
+    it('makes sure that plugins do not override module and project root components', async () => {
+        const html = await $fetch('/')
 
+        expect(html).not.toContain('Plugins are not allowed to override module nor project root components. Use slot mechanism instead.')
+    })
+
+    it('autoimports components added by plugins', async () => {
+        const html = await $fetch('/')
+
+        expect(html).toContain('Auto imported plugin component ')
+    })
+
+    it('injects pluginConfig.json to runtime config', async () => {
+        const html = await $fetch('/')
+
+        expect(html).toContain('RuntimeConfig | testPluginConfig1: Plugin-config 1')
+    })
+
+    it('overrides plugin config from nuxt.config', async () => {
+        const html = await $fetch('/')
+
+        expect(html).toContain('RuntimeConfig | testPluginConfig2: Plugin config 2 overridden by nuxt.config')
+    })
+
+    it('fills plugin slots with components from platform-plugins', async () => {
+        const html = await $fetch('/')
+
+        expect(html).toContain('Content from test plugin slot')
+    })
+
+    /*
+     * Runtime Configs
+     */
+    it('injects runtime config from modules default', async () => {
+        const html = await $fetch('/')
+
+        expect(html).toContain('RuntimeConfig | meta.category.title: Category - Hubble Demo')
+    })
+
+    /*
+     * Error handling
+     */
     it('throws 404 correctly', async () => {
         await expect(throw404()).rejects.toThrow('404')
+    })
+
+    it('adds custom error page and override it', async () => {
+        try {
+            await throw404()
+        } catch (e: any) {
+            expect(e.data).toContain('Override Custom Error Page')
+        }
+    })
+
+    /*
+     * i18n
+     */
+    it('generates localized routes from locales/availableLocales.json', async () => {
+        const html = await fetch('/de')
+
+        expect(html.status).toBe(200)
+    })
+
+    it('resolves translations serverside based on localized route', async () => {
+        const html = await $fetch('/de')
+
+        expect(html).toContain('Test Übersetzung')
     })
 })
