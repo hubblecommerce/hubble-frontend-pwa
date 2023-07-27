19e710a12bf3904347984cbb37d30108de2f0dff
Lukas Heinze <heinze@digitalmanufaktur.com>
2023-07-27 14:24:05 +0200

    issue#156 updated themes in theme switch

--- src/theme/components/layout/LayoutHeader.vue
+++ src/theme/components/layout/LayoutHeader.vue
@@ -89,34 +89,7 @@ const colorMode = useColorMode()
 const themes = [
     'system',
     'light',
-    'dark',
-    'cupcake',
-    'bumblebee',
-    'emerald',
-    'corporate',
-    'synthwave',
-    'retro',
-    'cyberpunk',
-    'valentine',
-    'halloween',
-    'garden',
-    'forest',
-    'aqua',
-    'lofi',
-    'pastel',
-    'fantasy',
-    'wireframe',
-    'black',
-    'luxury',
-    'dracula',
-    'cmyk',
-    'autumn',
-    'business',
-    'acid',
-    'lemonade',
-    'night',
-    'coffee',
-    'winter'
+    'dark'
 ]
 
 const {
