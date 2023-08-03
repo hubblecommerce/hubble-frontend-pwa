b65c6ceab5d708a618db940ba0f79267d251746c
Michel Kix <kix@digitalmanufaktur.com>
2023-08-03 19:07:40 +0200

    Removed wishlist page and added addToCart button to wishlist off canvas

--- src/theme/components/wishlist/Wishlist.vue
+++ src/theme/components/wishlist/Wishlist.vue
@@ -4,24 +4,8 @@
             <MiscSkeleton color="bg-base-100" text size="small" :repeat="6" />
         </div>
         <div v-else-if="!loading && !error" class="flex flex-col gap-6">
-            <div class="lg:overflow-y-auto" :class="showFullWishlistList ? 'lg:max-h-full' : 'lg:max-h-96'">
+            <div class="lg:overflow-y-auto lg:max-h-full'">
                 <WishlistProductList />
-            </div>
-            <div v-if="wishlist?.products?.length > wishlistDisplayLimit" class="hidden lg:block text-center -mt-1" @click="toggleWishlistList()">
-                <div class="inline-block cursor-pointer">
-                    <template v-if="!showFullWishlistList">
-                        <span class="underline font-medium mr-2">{{ t('wishlist.list.show') }}</span> <span><ArrowDownIcon class="w-5 h-5 inline" /></span>
-                    </template>
-                    <template v-else>
-                        <span class="underline font-medium mr-2">{{ t('wishlist.list.hide') }}</span> <span><ArrowUpIcon class="w-5 h-5 inline" /></span>
-                    </template>
-                </div>
-            </div>
-
-            <div v-if="wishlist?.products.length > 0 && isInteractive" class="flex flex-col w-full gap-6">
-                <MiscLink no-prefetch to="/wishlist" class="btn btn-outline">
-                    {{ t('wishlist.actions.editWishlist') }}
-                </MiscLink>
             </div>
         </div>
         <div v-else-if="error && !loading">
@@ -33,20 +17,8 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { storeToRefs } from 'pinia'
-import { useI18n } from 'vue-i18n'
 import { useNuxtApp } from '#app'
-import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/vue/24/outline'
 import { useWishlist } from '#imports'
-
-const { t } = useI18n()
-
-interface WishlistProps {
-    isInteractive?: boolean
-}
-
-const props = withDefaults(defineProps<WishlistProps>(), {
-    isInteractive: true
-})
 
 const wishlistStore = useWishlist()
 const { wishlist, loading, error } = storeToRefs(wishlistStore)
@@ -59,42 +31,8 @@ onMounted(async () => {
     await getWishlist()
 })
 
-const wishlistDisplayLimit = ref(3)
-const showFullWishlistList = ref(wishlist?.value?.products != null && wishlist?.value?.products.length <= wishlistDisplayLimit.value)
-
-function toggleWishlistList () {
-    showFullWishlistList.value = !showFullWishlistList.value
-}
-
 const { $hblBus } = useNuxtApp()
 onMounted(() => {
     $hblBus.$emit('viewWishlist', { wishlist: wishlist.value })
 })
 </script>
-
-<i18n>
-{
-    "en": {
-        "wishlist.actions.checkout": "Checkout",
-        "wishlist.actions.offer": "Request shopping wishlist",
-        "wishlist.totals.title": "Summary",
-        "wishlist.coupons.title": "Coupon",
-        "wishlist.actions.editWishlist": "Edit Wishlist",
-        "wishlist.list.show": "Show all",
-        "wishlist.list.hide": "Don't show all",
-        "wishlist.product.articleNumber": "Article no.:",
-        "wishlist.empty": "Your wishlist is empty"
-    },
-    "de": {
-        "wishlist.actions.checkout": "Zur Kasse",
-        "wishlist.actions.offer": "Warenkorb anfragen",
-        "wishlist.totals.title": "Zusammenfassung",
-        "wishlist.coupons.title": "Gutschein",
-        "wishlist.actions.editWishlist": "Warekorb bearbeiten",
-        "wishlist.list.show": "Alle anzeigen",
-        "wishlist.list.hide": "Nicht alle anzeigen",
-        "wishlist.product.articleNumber": "Artikel-Nr.:",
-        "wishlist.empty": "Ihr Warenkorb ist leer"
-    }
-}
-</i18n>
--- src/theme/components/wishlist/WishlistProductList.vue
+++ src/theme/components/wishlist/WishlistProductList.vue
@@ -4,42 +4,49 @@
             <div
                 v-for="product in wishlist.products"
                 :key="product.index"
-                class="relative flex items-start p-3 border rounded"
+                class="relative flex flex-col gap-2 items-start p-3 border rounded bg-base-100 border-base-300"
             >
-                <div class="avatar indicator mr-4">
-                    <div class="w-16 h-16 border rounded border-base-300">
-                        <img :src="product.media?.url" :alt="product.name">
+                <div class="flex w-full">
+                    <div class="avatar indicator mr-4">
+                        <div class="w-16 h-16 border rounded border-base-300">
+                            <img :src="product.media?.url" :alt="product.name">
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-between w-full">
+                        <div class="w-full flex justify-between gap-2">
+                            <div class="w-full pr-12">
+                                <div class="font-bold leading-5">
+                                    {{ product.name }}
+                                </div>
+                                <div v-if="product.sku != null" class="text-xs leading-4 text-gray-500 font-medium mt-1">
+                                    {{ `${t('wishlist.product.articleNumber')} ${product.sku}` }}
+                                </div>
+                            </div>
+                            <div class="absolute right-0 top-0 btn btn-ghost w-13 h-13" @click="onRemoveProduct(product)">
+                                <TrashIcon class="w-5 h-5" />
+                            </div>
+                        </div>
+                        <div v-if="product.price.regularPrice != null" class="self-end w-full text-sm leading-5 text-right mt-1">
+                            <div v-if="product.price.specialPrice && product.price.specialPrice > 0" :class="{ 'line-through text-xs': product.price.specialPrice }">
+                                {{ formatPrice(product.price.specialPrice) }}
+                            </div>
+                            <div :class="{ 'text-secondary': product.price.specialPrice }">
+                                <template v-if="product.price.regularPrice < 0">
+                                    - {{ formatPrice(product.price.regularPrice * -1) }}
+                                </template>
+                                <template v-else-if="product.price.regularPrice > 0">
+                                    {{ formatPrice(product.price.regularPrice) }}
+                                </template>
+                                <template v-else />
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="flex flex-col justify-between w-full">
-                    <div class="w-full flex justify-between gap-2">
-                        <div class="w-full pr-12">
-                            <div class="font-bold leading-5">
-                                {{ product.name }}
-                            </div>
-                            <div v-if="product.sku != null" class="text-xs leading-4 text-gray-500 font-medium mt-1">
-                                {{ `${t('wishlist.product.articleNumber')} ${product.sku}` }}
-                            </div>
-                        </div>
-                        <div v-if="isInteractive" class="absolute right-0 top-0 btn btn-ghost w-13 h-13" @click="onRemoveProduct(product)">
-                            <TrashIcon class="w-5 h-5" />
-                        </div>
-                    </div>
-                    <div v-if="product.price.regularPrice != null" class="self-end w-full text-sm leading-5 text-right mt-1">
-                        <div v-if="product.price.specialPrice && product.price.specialPrice > 0" :class="{ 'line-through text-xs': product.price.specialPrice }">
-                            {{ formatPrice(product.price.specialPrice) }}
-                        </div>
-                        <div :class="{ 'text-secondary': product.price.specialPrice }">
-                            <template v-if="product.price.regularPrice < 0">
-                                - {{ formatPrice(product.price.regularPrice * -1) }}
-                            </template>
-                            <template v-else-if="product.price.regularPrice > 0">
-                                {{ formatPrice(product.price.regularPrice) }}
-                            </template>
-                            <template v-else />
-                        </div>
-                    </div>
-                </div>
+
+                <button :disabled="cartLoading" class="btn btn-sm btn-primary small h-4 w-full flex-shrink" @click="onClickAddToCart(1, product)">
+                    <span v-if="cartLoading" class="loading" />
+                    <span v-if="!cartLoading">Add to cart</span>
+                </button>
             </div>
         </div>
         <div v-else>
@@ -53,22 +60,17 @@ import { storeToRefs } from 'pinia'
 import { useI18n } from 'vue-i18n'
 import { TrashIcon } from '@heroicons/vue/24/outline'
 import { useNuxtApp } from '#app'
-import { useWishlist, useCurrency } from '#imports'
+import { useWishlist, useCurrency, useCart } from '#imports'
 import { HblProduct } from '@/utils/types'
 
 const { t } = useI18n()
 
-interface WishlistListProps {
-    isInteractive?: boolean
-}
-
-const props = withDefaults(defineProps<WishlistListProps>(), {
-    isInteractive: true
-})
-
 const wishlistStore = useWishlist()
-const { wishlist, loading, error } = storeToRefs(wishlistStore)
+const { wishlist, error: wishListError } = storeToRefs(wishlistStore)
 const { removeFromWishlist } = wishlistStore
+const cartStore = useCart()
+const { loading: cartLoading, error: cartError } = storeToRefs(cartStore)
+const { addToCart } = cartStore
 
 const { formatPrice } = useCurrency()
 
@@ -76,8 +78,16 @@ const { $hblBus } = useNuxtApp()
 async function onRemoveProduct (product: HblProduct) {
     await removeFromWishlist(product.id)
 
-    if (!error.value) {
+    if (!wishListError.value) {
         $hblBus.$emit('removeFromWishlist', { product })
+    }
+}
+
+async function onClickAddToCart (qty: number, product: HblProduct) {
+    await addToCart(qty, product.id)
+
+    if (!cartError.value) {
+        $hblBus.$emit('addToCart', { qty, product })
     }
 }
 </script>
