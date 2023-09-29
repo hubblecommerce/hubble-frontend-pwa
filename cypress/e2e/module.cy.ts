96ed9c350d756eeaa5d532937fdb20652f49bd09
Lukas Heinze <heinze@digitalmanufaktur.com>
2023-09-29 14:43:48 +0200

    Moved custom error page test from integration tests to e2e tests because nuxt error response changed

--- cypress/e2e/module.cy.ts
+++ cypress/e2e/module.cy.ts
@@ -19,4 +19,10 @@ describe('TailwindCSS and DaisyUI integration', () => {
             .should('have.css', 'color')
             .should('equal', 'rgb(0, 0, 0)')
     })
+
+    it('is possible to override MiscError.vue to create a custom error page', () => {
+        cy.request({ url: '/this-route-not-exists', failOnStatusCode: false }).its('status').should('equal', 404)
+        cy.visit('/this-route-not-exists', { failOnStatusCode: false })
+        cy.get('html').contains('Override Custom Error Page')
+    })
 })
