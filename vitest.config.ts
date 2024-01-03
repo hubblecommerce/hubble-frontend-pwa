1f707f739b82ed9ab3af25fa450f26a8a8ee10c9
Lukas Heinze <heinze@digitalmanufaktur.com>
2024-01-03 15:13:46 +0100

    vitest.config.ts: replaced deps.inline with server.deps.inline

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,10 @@ import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        deps: {
-            inline: [/@nuxt\/test-utils/]
+        server: {
+            deps: {
+                inline: [/@nuxt\/test-utils/]
+            }
         },
         exclude: [...configDefaults.exclude, '**/fixture/**']
     }
