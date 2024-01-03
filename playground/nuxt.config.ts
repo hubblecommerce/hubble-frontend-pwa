4a0a54c9e2f452a42f050ab453152851244f0e34
Lukas Heinze <heinze@digitalmanufaktur.com>
2024-01-03 15:07:11 +0100

    Moved shopware api client aliases from tsconfig files to nuxt.config files for playground and tests

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath } from 'url'
 import eslintPlugin from 'vite-plugin-eslint'
+// @ts-ignore
 import hubble from '..'
 
 // @ts-ignore
 export default defineNuxtConfig({
     modules: [
+        // @ts-ignore
         hubble
     ],
     nitro: {
@@ -20,6 +22,16 @@ export default defineNuxtConfig({
                 // @ts-ignore
                 '@hubblecommerce/hubble/platforms/shopware/api-client': fileURLToPath(new URL('../src/platforms/shopware/api-client/generated/index.ts', import.meta.url)),
                 '@hubblecommerce/hubble/platforms/shopware/request': fileURLToPath(new URL('../src/platforms/shopware/api-client/generated/core/request.ts', import.meta.url))
+            }
+        }
+    },
+    typescript: {
+        tsConfig: {
+            compilerOptions: {
+                paths: {
+                    '@hubblecommerce/hubble/platforms/shopware/api-client': ['../src/platforms/shopware/api-client/generated/index'],
+                    '@hubblecommerce/hubble/platforms/shopware/request': ['../src/platforms/shopware/api-client/generated/core/request']
+                }
             }
         }
     }
